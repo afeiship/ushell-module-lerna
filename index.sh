fe27d2f9741453ac30180811ed4dba923da60130
aric.zheng <1290657123@qq.com>
2025-07-14 15:09:27 +0800

    feat: add lnap

--- index.sh
+++ index.sh
@@ -5,6 +5,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias lna='lerna';
 alias lna-init='npx lerna init';
 alias lna-inid='npx lerna init --independent';
-alias lna-publish='lerna publish'
+alias lna-publish='lerna publish';
+alias lnap='lerna publish --no-private';
 
 unset ROOT_PATH;
